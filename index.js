10704121355ad6a7ce84c3d9061bf795f82fc474
shaharia <adeptshaharia@gmail.com>
2025-05-20 16:02:55 +0600

    add_reade_api

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const port = 5000;
+
+app.use(express.json()); // for application/json
+app.use(express.urlencoded({ extended: true })); // for application/x-www-form-urlencoded
 // create product schema
 const productSchema = new mongoose.Schema({
   name: {
@@ -35,13 +38,47 @@ async function connectDB() {
     process.exit(1);
   }
 }
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 // Connect to DB first, then start server
   app.listen(port,async () => {
     console.log(`Server is running on port ${port}`);
      await connectDB()
   });
+  app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+// create product
+app.post('/products', async (req, res) => {
+  const { name, price, description } = req.body;
+  const product = new Product({ name, price, description });
+  try {
+    await product.save();
+    res.status(201).send(product);
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+app.get('/products',async(req,res)=>{
+    try{
+        const products=await Product.find();
+        if(products.length===0){
+            return res.status(404).send({message:'No products found'});
+        }else if(products.length>0){
+            return res.status(200).send({message:'Products found',data:products});
+        }
+   
+    }catch(err){
+        res.status(500).send({message:err.message});
+    }
+})
+// app.post('./products',async(req,res)=>{ 
+// const {name,price,description}=req.body;
+// const product=new Product({name,price,description});
+// try{
+//     await product.save();
+//     res.status(201).send({message:'product added successfully',data:product});
+// }catch(err){
+//     res.status(400).send({massage:err.message})
+// }
+// })
 
